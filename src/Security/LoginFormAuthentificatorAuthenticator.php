e0934fb52fba2d5b998769cc6c978bb3157b7e45
cat <harry@babychou.me>
2022-10-06 16:54:53 +0200

    Added Response type to some function

--- src/Security/LoginFormAuthentificatorAuthenticator.php
+++ src/Security/LoginFormAuthentificatorAuthenticator.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -88,7 +89,7 @@ class LoginFormAuthentificatorAuthenticator extends AbstractFormLoginAuthenticat
         return $credentials['password'];
     }
 
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey): ?HttpFoundationResponse
     {
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
